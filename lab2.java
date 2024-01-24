53d7ed974ee758fe1be25753744e3a5310b027ef
Mahi <ahnafraismahi@gmail.com>
2024-01-24 15:22:13 +0600

    shopfactory class added

--- lab2.java
+++ lab2.java
@@ -15,4 +15,33 @@ class Square implements Shape {
     }
 }
 
+class ShapeFactory {
+   
+    private static ShapeFactory instance;
+
+
+    private ShapeFactory() {
+    }
+
+   
+    public static ShapeFactory getInstance() {
+        if (instance == null) {
+            instance = new ShapeFactory();
+        }
+        return instance;
+    }
+
+    public Shape getShape(String shapeType) {
+        if (shapeType == null) {
+            return null;
+        }
+        if (shapeType.equalsIgnoreCase("RECTANGLE")) {
+            return new Rectangle();
+        } else if (shapeType.equalsIgnoreCase("SQUARE")) {
+            return new Square();
+        }
+        return null;
+    }
+}
+
 
